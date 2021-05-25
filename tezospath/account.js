6f568e2b905094f026d371c751c51c0ec68f6582
Logu J <1691611+jjlogu@users.noreply.github.com>
2021-05-25 19:20:17 +0530

    Tezo tutorial

---
+++ tezospath/account.js
@@ -0,0 +1,25 @@
+let FAUCET_KEY;
+export default FAUCET_KEY = {
+  "mnemonic": [
+    "badge",
+    "mass",
+    "fame",
+    "clean",
+    "sunset",
+    "enhance",
+    "amazing",
+    "wisdom",
+    "trim",
+    "tent",
+    "fortune",
+    "phrase",
+    "sell",
+    "man",
+    "intact"
+  ],
+  "secret": "06091148f166045b81bd5d1acef9ac99b432464d",
+  "amount": "40219792893",
+  "pkh": "tz1NR2sAP7VbiSkNq5uX2wsJmUPnGBVQBFJn",
+  "password": "1KPo2qcAcz",
+  "email": "kxzgdmox.altfpidd@tezos.example.org"
+}
